6d751c74733f9dc6609b962c18a51131ee132bfa
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-23 11:46:11 -0300

    operaciones a y b

---
+++ Clase2/variables.py
@@ -0,0 +1,10 @@
+print("hola mundo en python desde la unida")
+
+entero= 10
+flotante= 10.5
+cadena= "hola mundo"
+booleano= True
+print(entero,flotante,cadena,booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+entero=20
+print(type(entero))
