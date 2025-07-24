9c03216a3e51987339b8c7e945865c087fa04867
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-24 00:50:13 -0300

    uso de comentarios, finalizado variables

--- Clase2/variables.py
+++ Clase2/variables.py
@@ -1,3 +1,16 @@
+'''
+Esto es un comentario de varias lineas
+Puedes escribir lo que quieras y no se ejecutara en el programa.
+
+Consequat aute irure et sit. Ad veniam cillum laboris cupidatat do.
+Ut in in cillum ipsum cillum reprehenderit ut sunt dolor est occaecat.
+Et fugiat do fugiat irure aliqua adipisicing magna elit dolor mollit quis laborum eu.
+Eiusmod adipisicing est irure amet amet enim velit officia sunt non do.
+Sint et commodo labore ut commodo culpa commodo elit minim.
+Mollit mollit aute proident consequat exercitation nulla voluptate sit voluptate sint aliquip culpa.
+'''
+
+
 print("hola mundo en python desde la unida")
 
 entero= 10
@@ -6,5 +19,23 @@ cadena= "hola mundo"
 booleano= True
 print(entero,flotante,cadena,booleano)
 print(type(entero), type(flotante), type(cadena), type(booleano))
-entero=20
-print(type(entero))
+
+
+# Esto es un comentario de una linea
+# Sit cillum officia esse esse cupidatat minim sint do sunt aliquip dolore tempor occaecat.
+
+# Operaciones basicas con variables
+# Suma, resta, multiplicacion y division
+
+
+print("Sumando dos numeros: ")
+a=10
+b=20
+print("Suma de dos numeros ",a+b)
+print("Restando dos numeros: ")
+print("Resta de dos numeros ",a-b)
+print("Multiplicando dos numeros: ")
+print("Multiplicacion de dos numeros ",a*b)
+b=5
+print("Dividiendo dos numeros: ")
+print("Division de dos numeros ",a/b)
