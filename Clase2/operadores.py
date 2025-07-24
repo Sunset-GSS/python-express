13630b20961d38ae50eabaf5ab1a78467e8aa220
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-24 01:10:59 -0300

    finalizado operadores

---
+++ Clase2/operadores.py
@@ -0,0 +1,27 @@
+'''
+a==b
+a!=b
+a>b
+a<b
+a>=b
+a<=b
+'''
+
+r=100 == 90 
+print(r)
+r=100 != 90 
+print(r)
+
+a=100
+b=200
+r=a > b 
+print("Es mayor",r)
+r=a < b 
+print("Es menor",r)
+
+a=18 #Se pide al usuario que ingrese un numero
+b=17
+r=a >= b 
+print(r)
+r=a <= b 
+print(r)
