9ce5a48e30a7da8fc1d3323fa628950f0618fdc5
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-24 01:24:40 -0300

    operadores aritmeticos

---
+++ Clase2/aritmeticos.py
@@ -0,0 +1,33 @@
+'''
+operaciones aritméticas basicas en Python
+sumamos, restamos, multiplicamos y dividimos dos numeros
+c=a + b
+d= a - b
+e= a * b
+f= a / b
+g= a%b
+h= a**b
+i= a//b
+'''
+a = 20
+b = 18
+r = 0 # Inizializamos r para evitar errores
+r = a + b
+print("Suma:", r)
+r = a - b
+print("Resta:", r)
+
+e = a * b
+print("Multiplicación:", e)
+f = a / b
+print("División:", f)
+
+g = a % b
+print("Módulo:", g)
+h = a ** b
+print("Exponente:", h)
+i = a // b
+print("División entera:", i)
+
+print("Operaciones finalizadas")
+# Operadores de asignación
