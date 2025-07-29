18aac49beb533c5840d74d86c15a2c8eab6efff0
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-29 14:30:44 -0300

    condicionales if-elif-else

---
+++ Clase2/condicionales.py
@@ -0,0 +1,22 @@
+'''
+Condicionales if
+'''
+dato = int(input("Ingrese un número: "))
+
+# Ejemplo de uso de if
+if dato % 2 == 0:
+    print("El número es par")
+
+# Ejemplo de uso de if-else
+if dato % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
+
+# Ejemplo de uso de if-elif-else
+if dato > 0:
+    print("El número es positivo")
+elif dato < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
