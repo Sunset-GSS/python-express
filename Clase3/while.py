63866b9703c0cc982a477a52990dc23870005b1b
Dominick Maldonado <dominickmaldonado31@gmail.com>
2025-07-29 16:15:24 -0300

    while

---
+++ Clase3/while.py
@@ -0,0 +1,9 @@
+'''
+Extructura de control while
+'''
+
+contador = 0 # Inizializar el contador
+while contador < 5:
+    print("El contador es:", contador) # Imprimir el valor del contador
+    contador += 1 # Incrementar el contador en 1
+print("Fin del bucle while") # Mensaje al finalizar el bucle
